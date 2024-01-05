e1735d65c3f11719a36d6b9eedc6457c5f5a7a25
Emily Yu <121976605+00eemsy@users.noreply.github.com>
2024-01-04 19:54:35 -0800

    Update code.js

--- code.js
+++ code.js
@@ -1,53 +1,60 @@
-var prev = {class: "", id: ""};
-var curr = {class: "", id: ""};
+var prev = {class: ""};
+var curr = {class: ""};
 var round = 1;
 
 var score = 0;
 
-function buttonClicked(event) {
+function imgClicked(event) {
 
     let suite = event.target.className;
 
     // "flip over" card
 
-    if (suite === "club") {
-        event.target.querySelector("img").src = "https://i.pinimg.com/736x/2e/c2/7e/2ec27e16f730308d3fdd58a824341afb.jpg";
-    } else if (suite === "spade") {
-        event.target.querySelector("img").src = "https://i.pinimg.com/736x/88/c8/ac/88c8acf22b16900e5202a29250841e03.jpg";
-    } else if (suite === "diamond") {
-        event.target.querySelector("img").src = "https://i.pinimg.com/736x/9b/6d/c0/9b6dc0fa6f198d0e3ec3e61abed20ea4.jpg";
+    if (suite === "headphones") {
+        event.target.src = "https://i.pinimg.com/564x/41/bb/8a/41bb8ac6926de03470ac8ca5a89884e8.jpg";
+    } else if (suite === "tulip") {
+        event.target.src = "https://i.pinimg.com/564x/d8/49/68/d8496885cde471470faa0861bd5d2b28.jpg";
+    } else if (suite === "baobing") {
+        event.target.src = "https://i.pinimg.com/564x/3e/7b/5c/3e7b5cf42914a09ae14478327eca5272.jpg";
     }
 
     if (round === 1) {
         prev.class = `${suite}`;
-        prev.id = `${event.target.id}`;
-
-        document.getElementById("prevID").innerHTML = prev.id;
-
         round ++; 
     } else { // round 2!
         curr.class = `${suite}`;
-        curr.id = `${event.target.id}`;
 
-        document.getElementById("currID").innerHTML = curr.id;
-
-        if ((prev.class === curr.class) && (prev.id != curr.id)) {
+        if (prev.class === curr.class) {
             score ++;
             document.getElementById("myScore").innerHTML = `${score}`;
 
-            prev = {class: "", id: ""};
-            curr = {class: "", id: ""};
+            prev.class = "";
+            curr.class = "";
+
+            round = 1;
         } else {
             prev.class = `${curr.class}`;
-            prev.id = `${curr.id}`;
+            // no change to round
         }
-
-        round = 1;
     }
+
+    event.target.setAttribute("onclick", null);
 
 }
 
+function shuffleDeck() {
+    let suites = ['headphones', 'headphones', 'tulip', 'tulip', 'baobing', 'baobing'];
 
-const but = document.querySelector('button');
-but.addEventListener('click', buttonClicked);
+    for (let i of document.querySelectorAll('img')) {
+        let suite_rand = Math.floor(Math.random() * suites.length);
+
+        i.setAttribute("class", `${suites[suite_rand]}`);
+        suites.splice(suite_rand, 1);
+    }
+}
+
+addEventListener('load', shuffleDeck);
+
+const i = document.querySelector('img');
+i.addEventListener('click', imgClicked);
 
